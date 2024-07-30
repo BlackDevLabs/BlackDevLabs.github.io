4d119f0d92bf8a51bc980838aed95a7bb83243a3
BlackDev <tjorgefaerber@outlook.com>
2024-07-30 18:40:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ document.addEventListener("DOMContentLoaded", function() {
         window.scrollTo(0, 0);
     }, 1000);
 });
+let count = 0;
+
+function formatCount(count) {
+    if (count >= 1e6) {
+        return '1M';
+    } else if (count >= 1e3) {
+        let kPart = Math.floor(count / 1000);
+        let rest = count % 1000;
+        return `${kPart}k ${rest.toString().padStart(3, '0')}`;
+    } else {
+        return count.toString().padStart(3, '0');
+    }
+}
+
+function updateCounter() {
+    count += 2;
+    document.getElementById('counter').innerText = formatCount(count);
+}
+
+
+setInterval(updateCounter, 120000);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('counter').innerText = formatCount(count);
+});
